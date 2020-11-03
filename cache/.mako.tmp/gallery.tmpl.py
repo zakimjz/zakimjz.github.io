d97f056c14c579c7b88491be8e4c79d3b6ce1aa5
Mohammed J. Zaki <mjzaki@gmail.com>
2020-11-02 20:52:38 -0500

    Nikola auto commit.
    Nikola version: 8.1.1

--- cache/.mako.tmp/gallery.tmpl.py
+++ cache/.mako.tmp/gallery.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1604100938.9118068
+_modified_time = 1604368357.5432987
 _enable_loop = True
 _template_filename = '/usr/local/lib/python3.8/dist-packages/nikola/data/themes/base/templates/gallery.tmpl'
 _template_uri = 'gallery.tmpl'
@@ -36,35 +36,35 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        title = context.get('title', UNDEFINED)
-        enable_comments = context.get('enable_comments', UNDEFINED)
-        photo_array_json = context.get('photo_array_json', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
         photo_array = context.get('photo_array', UNDEFINED)
-        def sourcelink():
-            return render_sourcelink(context._locals(__M_locals))
-        lang = context.get('lang', UNDEFINED)
-        len = context.get('len', UNDEFINED)
-        translations = context.get('translations', UNDEFINED)
-        url_replacer = context.get('url_replacer', UNDEFINED)
-        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
-        def content():
-            return render_content(context._locals(__M_locals))
-        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
-        post_helper = _mako_get_namespace(context, 'post_helper')
-        comments = _mako_get_namespace(context, 'comments')
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
-        _link = context.get('_link', UNDEFINED)
-        folders = context.get('folders', UNDEFINED)
-        permalink = context.get('permalink', UNDEFINED)
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        def content():
+            return render_content(context._locals(__M_locals))
+        folders = context.get('folders', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        gallery_path = context.get('gallery_path', UNDEFINED)
+        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        photo_array_json = context.get('photo_array_json', UNDEFINED)
+        translations = context.get('translations', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        def sourcelink():
+            return render_sourcelink(context._locals(__M_locals))
+        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
+        lang = context.get('lang', UNDEFINED)
+        len = context.get('len', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
+        post_helper = _mako_get_namespace(context, 'post_helper')
         def extra_js():
             return render_extra_js(context._locals(__M_locals))
+        title = context.get('title', UNDEFINED)
         post = context.get('post', UNDEFINED)
+        url_replacer = context.get('url_replacer', UNDEFINED)
+        enable_comments = context.get('enable_comments', UNDEFINED)
         parent = context.get('parent', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
-        gallery_path = context.get('gallery_path', UNDEFINED)
-        crumbs = context.get('crumbs', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -109,22 +109,22 @@ def render_sourcelink(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
+        photo_array = context.get('photo_array', UNDEFINED)
+        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
+        lang = context.get('lang', UNDEFINED)
         def content():
             return render_content(context)
-        title = context.get('title', UNDEFINED)
-        enable_comments = context.get('enable_comments', UNDEFINED)
-        thumbnail_size = context.get('thumbnail_size', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
-        photo_array = context.get('photo_array', UNDEFINED)
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
         folders = context.get('folders', UNDEFINED)
-        permalink = context.get('permalink', UNDEFINED)
-        post = context.get('post', UNDEFINED)
-        lang = context.get('lang', UNDEFINED)
         ui = _mako_get_namespace(context, 'ui')
-        crumbs = context.get('crumbs', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        post = context.get('post', UNDEFINED)
+        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
         url_replacer = context.get('url_replacer', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        enable_comments = context.get('enable_comments', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(ui.breadcrumbs(crumbs)))
@@ -199,16 +199,16 @@ def render_content(context,**pageargs):
 def render_extra_head(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        post_helper = _mako_get_namespace(context, 'post_helper')
-        _link = context.get('_link', UNDEFINED)
         def extra_head():
             return render_extra_head(context)
-        post = context.get('post', UNDEFINED)
         lang = context.get('lang', UNDEFINED)
-        parent = context.get('parent', UNDEFINED)
-        translations = context.get('translations', UNDEFINED)
         len = context.get('len', UNDEFINED)
         gallery_path = context.get('gallery_path', UNDEFINED)
+        post_helper = _mako_get_namespace(context, 'post_helper')
+        post = context.get('post', UNDEFINED)
+        translations = context.get('translations', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        parent = context.get('parent', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer(str(parent.extra_head()))
@@ -236,8 +236,8 @@ def render_extra_head(context,**pageargs):
 def render_extra_js(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        photo_array_json = context.get('photo_array_json', UNDEFINED)
         thumbnail_size = context.get('thumbnail_size', UNDEFINED)
+        photo_array_json = context.get('photo_array_json', UNDEFINED)
         def extra_js():
             return render_extra_js(context)
         __M_writer = context.writer()
---
+++ pages/courses/datamining/assign6.md
@@ -0,0 +1,96 @@
+<!--
+.. title: CSCI4390-6390 Assign6
+.. slug: dm_assign6
+.. date: 2020-11-02 20:23:01 UTC-04:00
+.. tags: 
+.. category: 
+.. link: 
+.. description: 
+.. has_math: True
+.. type: text
+-->
+
+# Assign6: Support Vector Machines
+
+**Due Date**: Nov 9, before midnight (11:59:59PM, Alofi Time; GMT-11)
+
+
+You will use the 
+[Appliances energy prediction data set](https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction#).
+You should ignore the first attribute, which is a date-time variable,
+and you should also remove the last attribute, which is a duplicate of
+the previous one. Use the first attribute (after removing the
+date-time variable), which denotes the
+**Appliances Energy Use**, as the response variable, with the remaining
+attributes as predictor variables. 
+
+Note that the **Appliances Energy Use** attribute takes values in the
+range $[10,1080]$. However, for binary classification, we need only two
+values, so for the purpose of this assignment you should consider energy
+use less than or equal to 50 as the positive class (1), and energy use
+higher than 50 as negative class (0). 
+
+You should shuffle the data points before selecting 70% of the data
+training and 30% for testing, so that there is an equal mix of the
+classes in both.
+
+You will implement the dual SVM Algorithm 21.1 (Chapter 21, page 540).
+You must implement both the "hinge" and "quadratic" loss cases, which
+will be command line options.
+
+##CSCI4390
+
+You must implement two kernels, namely, both linear and Gaussian.
+
+##CSCI6390
+
+You must implement three kernels, namely, linear, Gaussian, and polynomial.
+
+---
+
+## What to submit
+
+* Write a script named as **Assign6.py**, which will be run as 
+      
+   Assign6.py FILENAME LOSS C EPS MAXITER KERNEL KERNEL_PARAM
+   
+ FILENAME is the datafile name,  LOSS is either the string "hinge" or
+ "quadratic", C is the regularization constant, EPS is the convergence
+ threshold, MAXITER is the max number of iterations to perform (in
+ caseyou do not get convergence within EPS), KERNEL is one of the
+ strings "linear", "gaussian" or "polynomial", and finally KERNEL_PARAM
+ is either a float that represents the spread $\sigma^2$ for gaussian
+ kernel, or it is a comma separated pair q,c for the polynomial kernel (see Eq 5.9 on page 144 in the book), with $q$ being the degree and $c$ the kernel constant (note that polynomial kernel is 
+ only for CSCI6390).
+
+You should implement the algorithms using NumPy; you cannot use any other library.
+
+Your script should print out the support vectors on the training set,
+and the final
+accuracy value on the test data, where you use 70% of the data for
+training and 30% for testing.
+
+Note that accuracy is defined as the fraction of correct class label
+predictions. So for each test point, you should predict its class as the
+sign of the hyperplane equation (see Eq 21.3).
+ Divide the number of correct predictions by the test data
+size to get the accuracy. Report the
+results for the combination of LOSS, C, KERNEL and KERNEL_PARAM values
+that yields the best accuracy.
+
+* Submit a PDF file named Assign6.pdf that should include your answers
+ to each of the questions (just cut and paste the output from python).
+ **Failure the submit the PDF will result in lost points.** 
+
+* Submit the scripts and pdf file via submitty
+
+---
+
+## Policy on Academic Honesty
+
+You are free to discuss how to tackle the assignment, but all coding
+must be your own. Please do not copy or modify code from anyone else,
+including code on the web. Any students caught violating the academic
+honesty principle will get an automatic F grade on the course and will
+be referred to the dean of students for disciplinary action.
+
