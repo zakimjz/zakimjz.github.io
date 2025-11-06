01e0a9a92ddab534650940c3c9b27b010d164da9
Mohammed J. Zaki <mjzaki@gmail.com>
2025-11-06 14:25:24 -0500

    Nikola auto commit.
    Nikola version: 8.3.3

--- cache/.mako.tmp/gallery.tmpl.py
+++ cache/.mako.tmp/gallery.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1761858052.8497057
+_modified_time = 1762457118.0171266
 _enable_loop = True
 _template_filename = '/usr/local/lib/python3.12/dist-packages/nikola/data/themes/base/templates/gallery.tmpl'
 _template_uri = 'gallery.tmpl'
@@ -36,34 +36,34 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        ui = _mako_get_namespace(context, 'ui')
+        gallery_path = context.get('gallery_path', UNDEFINED)
         translations = context.get('translations', UNDEFINED)
-        permalink = context.get('permalink', UNDEFINED)
-        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
-        def sourcelink():
-            return render_sourcelink(context._locals(__M_locals))
-        parent = context.get('parent', UNDEFINED)
-        crumbs = context.get('crumbs', UNDEFINED)
-        def extra_head():
-            return render_extra_head(context._locals(__M_locals))
-        photo_array = context.get('photo_array', UNDEFINED)
-        _link = context.get('_link', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        enable_comments = context.get('enable_comments', UNDEFINED)
+        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
         thumbnail_size = context.get('thumbnail_size', UNDEFINED)
-        len = context.get('len', UNDEFINED)
-        title = context.get('title', UNDEFINED)
-        lang = context.get('lang', UNDEFINED)
-        photo_array_json = context.get('photo_array_json', UNDEFINED)
+        parent = context.get('parent', UNDEFINED)
         post = context.get('post', UNDEFINED)
+        title = context.get('title', UNDEFINED)
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        len = context.get('len', UNDEFINED)
+        lang = context.get('lang', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        folders = context.get('folders', UNDEFINED)
         def extra_js():
             return render_extra_js(context._locals(__M_locals))
-        folders = context.get('folders', UNDEFINED)
-        gallery_path = context.get('gallery_path', UNDEFINED)
-        enable_comments = context.get('enable_comments', UNDEFINED)
-        post_helper = _mako_get_namespace(context, 'post_helper')
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
         comments = _mako_get_namespace(context, 'comments')
+        photo_array = context.get('photo_array', UNDEFINED)
+        photo_array_json = context.get('photo_array_json', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
+        post_helper = _mako_get_namespace(context, 'post_helper')
+        def sourcelink():
+            return render_sourcelink(context._locals(__M_locals))
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -108,20 +108,20 @@ def render_sourcelink(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        ui = _mako_get_namespace(context, 'ui')
         title = context.get('title', UNDEFINED)
-        permalink = context.get('permalink', UNDEFINED)
+        folders = context.get('folders', UNDEFINED)
         def content():
             return render_content(context)
-        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
-        post = context.get('post', UNDEFINED)
-        crumbs = context.get('crumbs', UNDEFINED)
-        folders = context.get('folders', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        photo_array = context.get('photo_array', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
         site_has_comments = context.get('site_has_comments', UNDEFINED)
+        crumbs = context.get('crumbs', UNDEFINED)
         thumbnail_size = context.get('thumbnail_size', UNDEFINED)
         enable_comments = context.get('enable_comments', UNDEFINED)
-        photo_array = context.get('photo_array', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
+        galleries_use_thumbnail = context.get('galleries_use_thumbnail', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
+        post = context.get('post', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(ui.breadcrumbs(crumbs)))
@@ -198,16 +198,16 @@ def render_content(context,**pageargs):
 def render_extra_head(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        translations = context.get('translations', UNDEFINED)
-        lang = context.get('lang', UNDEFINED)
-        parent = context.get('parent', UNDEFINED)
-        post = context.get('post', UNDEFINED)
-        gallery_path = context.get('gallery_path', UNDEFINED)
+        len = context.get('len', UNDEFINED)
         def extra_head():
             return render_extra_head(context)
-        post_helper = _mako_get_namespace(context, 'post_helper')
+        lang = context.get('lang', UNDEFINED)
+        translations = context.get('translations', UNDEFINED)
         _link = context.get('_link', UNDEFINED)
-        len = context.get('len', UNDEFINED)
+        gallery_path = context.get('gallery_path', UNDEFINED)
+        post_helper = _mako_get_namespace(context, 'post_helper')
+        parent = context.get('parent', UNDEFINED)
+        post = context.get('post', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer(str(parent.extra_head()))
--- pages/courses/datamining.md
+++ pages/courses/datamining.md
@@ -61,16 +61,16 @@ below.
 | Oct 20     | Clustering Assessment (Chapter 17)                                | [lecture14](http://www.cs.rpi.edu/~zaki/DMCOURSE/lectures/lecture14.pdf) |
 | Oct 23     | Guest Lecture (Graph Clustering)                                  | |
 | Oct 27     | Bayes Classifier (Chapters 18)                                    | [lecture15](http://www.cs.rpi.edu/~zaki/DMCOURSE/lectures/lecture15.pdf) |
-| Oct 30     | Decision Trees (Chapter 19)                                    | [lecture16](http://www.cs.rpi.edu/~zaki/DMCOURSE/lectures/lecture16.pdf) |
-| Nov 03     | **EXAM II**                                                       ||
-| Nov 06     | Support Vector Machines I (Chapter 21)                                 ||
-| Nov 10     | Support Vector Machines II (Chapter 21)                                    ||
+| Oct 30     | Decision Trees (Chapter 19)                                       | [lecture16](http://www.cs.rpi.edu/~zaki/DMCOURSE/lectures/lecture16.pdf) |
+| Nov 03     | **EXAM II**                                                       | |
+| Nov 06     | Support Vector Machines I (Chapter 21)                            | [lecture17](http://www.cs.rpi.edu/~zaki/DMCOURSE/lectures/lecture17.pdf) |
+| Nov 10     | Support Vector Machines II (Chapter 21)                           ||
 | Nov 13     | Linear Regression (Chapter 23)                                    ||
-| Nov 17     | Logistic Regression (Chapter 24)                            ||
-| Nov 20     | Neural Networks I (Chapter 25)                           ||
+| Nov 17     | Logistic Regression (Chapter 24)                                  ||
+| Nov 20     | Neural Networks I (Chapter 25)                                    ||
 | Nov 24     | **NO CLASS** (Thanksgiving)                                       ||
 | Nov 27     | **NO CLASS** (Thanksgiving)                                       ||
-| Dec 01     | Neural Networks II (Chapters 25)                          ||
-| Dec 04     | Classification Assessment (Chapters 22)                        ||
+| Dec 01     | Neural Networks II (Chapters 25)                                  ||
+| Dec 04     | Classification Assessment (Chapters 22)                           ||
 | Dec 08     | Regression Assessment (Chapter 27)                                ||
 | Dec 11     | **EXAM III**                                                      ||
